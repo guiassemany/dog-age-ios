fd1ceaa187a0c01b025745ab51aa2393d5c7ddc7
Guilherme Assemany <guilhermeassemany@hotmail.com>
2017-01-08 21:14:40 -0200

    Initial Commit

---
+++ IdadeCachorro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IdadeCachorro
+//
+//  Created by Guilherme Assemany on 08/01/17.
+//  Copyright © 2017 Guilherme Assemany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
